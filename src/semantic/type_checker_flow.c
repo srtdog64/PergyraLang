7d8f045473a92ca9a735db8e749c20ed1df37199
Dongwoo Jeong <doogwoo@gmail.com>
2026-07-14 16:50:38 +0900

    fix(ci): include semantic timing clock contract

--- src/semantic/type_checker_flow.c
+++ src/semantic/type_checker_flow.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "type_checker_internal.h"
 #include "type_checker_ownership_internal.h"
 #include "type_checker_ownership_consumers_internal.h"
