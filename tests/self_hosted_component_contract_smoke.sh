2d5f50c4664dfedbe9f31ea9ebb0aa35a75217c0
Dongwoo Jeong <doogwoo@gmail.com>
2026-07-16 16:17:23 +0900

    selfhost: keep source-unit parsing typed

--- tests/self_hosted_component_contract_smoke.sh
+++ tests/self_hosted_component_contract_smoke.sh
@@ -586,7 +586,9 @@ require_text "src/self_hosted/parser/run_owner.pgy" "ParseRootProgram(source_pat
 reject_text "src/self_hosted/parser/main.pgy" 'import "source_path_owner.pgy";'
 reject_text "src/self_hosted/parser/main.pgy" 'import "program_parse_owner.pgy";'
 require_text "src/self_hosted/parser/main.pgy" "RunParserFromArgs(Args())"
-require_text "src/self_hosted/parser/run_owner.pgy" "ParseSourceUnitProgram(args[1])"
+require_text \
+    "src/self_hosted/parser/run_owner.pgy" \
+    "ParseProgramBuildWithImportPolicy(args[1], false).tree_text"
 require_text "src/self_hosted/parser/run_owner.pgy" "ParseRootProgram(source_path)"
 require_text "src/self_hosted/parser/run_owner.pgy" 'args[0] == "--source-unit-ast"'
 require_text "tests/self_hosted/parity/completeness_ledger.sh" \
